d10df546f630548469dd8cbe91fb5a49e1e321be
miguelhasbun <miguelneptaly@gmail.com>
2017-05-28 23:28:00 -0600

    Gran avance para los restaurantes

--- app/src/main/java/com/example/miguelpaz/eatco/Menu.java
+++ app/src/main/java/com/example/miguelpaz/eatco/Menu.java
@@ -1,7 +1,9 @@
 package com.example.miguelpaz.eatco;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,4 +12,29 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
+
+    public void callusa(View view){
+        Intent i= new Intent(this, Americano.class);
+        startActivity(i);
+    }
+    public void callhonduras(View view){
+        Intent i= new Intent(this, Tipico.class);
+        startActivity(i);
+    }
+
+    public void callmexico(View view){
+        Intent i= new Intent(this, Mexicano.class);
+        startActivity(i);
+    }
+
+    public void callasiatico(View view){
+        Intent i= new Intent(this, Asiatico.class);
+        startActivity(i);
+    }
+
+    public void callitaliano(View view){
+        Intent i= new Intent(this, Italiano.class);
+        startActivity(i);
+    }
+
 }
---
+++ app/src/main/java/com/example/miguelpaz/eatco/Tipico.java
@@ -0,0 +1,13 @@
+package com.example.miguelpaz.eatco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tipico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipico);
+    }
+}
