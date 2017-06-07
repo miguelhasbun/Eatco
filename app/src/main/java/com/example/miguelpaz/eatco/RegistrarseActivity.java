b85fe34f2e91cac4ea2578af525ded9f87afcae2
davidxdiaz <davidxdiaz@unitec.edu>
2017-06-07 17:09:09 -0600

    agregado archivos

---
+++ app/src/main/java/com/example/miguelpaz/eatco/RegistrarseActivity.java
@@ -0,0 +1,82 @@
+package com.example.miguelpaz.eatco;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class RegistrarseActivity extends AppCompatActivity {
+    EditText txtNombre, txtPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse);
+        txtNombre = (EditText) findViewById(R.id.txtNombre);
+        txtPass = (EditText) findViewById(R.id.txtPass);
+
+
+    }
+
+    public void callregistrarse(View view) {
+        String user = txtNombre.getText().toString();
+        String pass = txtPass.getText().toString();
+        //Toast.makeText(this, "LLamado registrar", Toast.LENGTH_LONG).show();
+        try{
+            if(usuario.existe(user)==null){
+
+
+                usuario.add(user,pass);
+                Toast.makeText(this, "Usuario registrado correctamente.", Toast.LENGTH_LONG).show();
+                usuario.setLoggedUser(usuario.existe(user));
+                Intent inte = new Intent(this, MainActivity.class);
+                startActivity(inte);
+            }
+            else{
+                Toast.makeText(this, "Ya existe un usuario con este nombre.", Toast.LENGTH_LONG).show();
+            }
+        }catch(IOException e){
+            try {
+                usuario.add(user, pass);
+            } catch (IOException ex) {
+                Logger.getLogger(RegistrarseActivity.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("Error en registro "+e.getMessage());
+        }
+
+
+
+    }
+
+    public void calliniciar(View view) {
+        String user = txtNombre.getText().toString();
+        String pass = txtPass.getText().toString();
+
+        try {
+            usuario login = usuario.verificar(user, pass);
+
+            if (login != null) {
+                usuario.setLoggedUser(login);
+                Intent inte = new Intent(this, MainActivity.class);
+                startActivity(inte);
+                //System.out.println("LOGGED USER: " + usuario.getLoggedUser());
+            } else {
+                Toast.makeText(this, " Usuario y/o contraseña incorrectos.\n"
+                        + "Debe crear el usuario o intentar con otro usuario y contraseña", Toast.LENGTH_LONG).show();
+            }
+        }
+        catch(IOException e){
+            System.out.println("Error log in "+e.getMessage()+" ");
+            e.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(RegistrarseActivity.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
--- app/src/main/java/com/example/miguelpaz/eatco/usuario.java
+++ app/src/main/java/com/example/miguelpaz/eatco/usuario.java
@@ -15,77 +15,86 @@ import java.io.ObjectOutputStream;
 
 public class usuario {
 
-    private String usuario,password,email;
+    private String usuario,password;
     private int puntos;
     public static ArrayList<usuario>usuarios=new ArrayList<>();
-    public static usuario loggedPlayer;
+    public static usuario loggedUser;
     private ArrayList<Historial>historial;
 
-    private static FileOutputStream fo;
-    private static ObjectOutputStream oo;
-
-
-    public usuario(String user, String pass,String e){
+    public usuario(String user, String pass){
         this.usuario=user;
         this.password=pass;
-        this.email=e;
         this.historial = new ArrayList<>();
         this.puntos=0;
     }
 
-    public static void creandoFolder() throws FileNotFoundException, IOException{
-        try{
-            //1- Asegurar que el folder Players exista
-            File f = new File("Users");
-            f.mkdir();
-            //2- Instanciar los RAFs dentro de company
-            //rplayers = new RandomAccessFile("Players/players.pl", "rw");
-            fo = new FileOutputStream("Users/user.pl");
-            oo = new ObjectOutputStream(fo);
-        }catch(IOException e){
-            System.out.println("No deberia de pasar esto");
-        }
 
-    }
 
-    public static void guardarUsuarios()throws IOException{
+    public static void add(String user, String pass)throws IOException{
+        usuarios.add(new usuario(user, pass));
+        FileOutputStream fo = new FileOutputStream("user.us");
+        ObjectOutputStream oo = new ObjectOutputStream(fo);
         oo.writeObject(usuarios);
     }
-    public static void cargarUsuarios() throws IOException, ClassNotFoundException{
-        FileInputStream fi = new FileInputStream("Users/user.pl");
-        ObjectInputStream oi = new ObjectInputStream(fi);
-        usuarios.add((usuario) (oi.readObject()));
-    }
 
-    public static void add(String user, String pass,String e){
-        usuario newUser = new usuario(user,pass,e);
-        setLoggedPlayer(newUser);
-        usuarios.add(newUser);
-    }
-    public static usuario existe(String user){
-        for(usuario u: usuarios){
-            if(u.getUsername().equals(user))
+    public static usuario existe(String user) throws FileNotFoundException, IOException{
+        try{
+            FileInputStream fi = new FileInputStream("user.us");
+            ObjectInputStream oi = new ObjectInputStream(fi);
+            usuarios = (ArrayList<usuario>)oi.readObject();
+        } catch (FileNotFoundException ex) {
+
+        } catch (IOException | ClassNotFoundException ex) {
+            FileOutputStream fo= new FileOutputStream("user.us");
+            ObjectOutputStream oo = new ObjectOutputStream(fo);
+            oo.writeObject(usuarios);
+            //Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        for (usuario u : usuarios){
+            if (u != null){
                 return u;
+
+            }
         }
         return null;
     }
 
-    public static usuario verificar(String user, String pass){
-        usuario u = existe(user);
-        return u.getPassword().equals(pass) ? u:null;
+    public static usuario verificar(String text, String passw) throws IOException, ClassNotFoundException {
+        // FileOutputStream fo = new FileOutputStream("player.pl");
+        // ObjectOutputStream oo = new ObjectOutputStream(fo);
+        //oo.writeObject(players);
+        try{
+            FileInputStream fi = new FileInputStream("user.us");
+            ObjectInputStream oi = new ObjectInputStream(fi);
+            usuarios=(ArrayList<usuario>)oi.readObject();
+            for(usuario u:usuarios){
+                if (text.equals(u.getUsername())){
+                    if(passw.equals(u.getPassword())){
+                        return u;
+                    }
+                }
+            }return null;
+        }catch(FileNotFoundException e){
+            FileOutputStream fo = new FileOutputStream("user.us");
+            ObjectOutputStream oo = new ObjectOutputStream(fo);
+            oo.writeObject(usuarios);
+            return null;
+        }
+
     }
 
     public static void addPuntos(String user){
-        int pts = getLoggedPlayer().getPuntos();
-        getLoggedPlayer().setPuntos(pts+3);
+        int pts = getLoggedUser().getPuntos();
+        getLoggedUser().setPuntos(pts+3);
     }
 
-    public static usuario getLoggedPlayer(){
-        return loggedPlayer;
+    public static usuario getLoggedUser(){
+        return loggedUser;
     }
 
-    public static void setLoggedPlayer(usuario user){
-        loggedPlayer = user;
+    public static void setLoggedUser(usuario user){
+        loggedUser = user;
     }
 
     public String getUsername() {
@@ -96,13 +105,6 @@ public class usuario {
         this.usuario = username;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String correo) {
-        this.email = correo;
-    }
 
     public String getPassword() {
         return password;
