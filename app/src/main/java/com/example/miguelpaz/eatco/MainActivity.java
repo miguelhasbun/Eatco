677312f23e69108924cacef4759fd090fb7feb7c
miguelhasbun <miguelneptaly@gmail.com>
2017-06-13 23:34:01 -0600

    Nosotros terminado

--- app/src/main/java/com/example/miguelpaz/eatco/MainActivity.java
+++ app/src/main/java/com/example/miguelpaz/eatco/MainActivity.java
@@ -27,4 +27,9 @@ public class MainActivity extends AppCompatActivity {
         Intent i= new Intent(this, Configuracion.class);
         startActivity(i);
     }
+
+    public void callnosotros(View view){
+        Intent i= new Intent(this, Nosotros.class);
+        startActivity(i);
+    }
 }
